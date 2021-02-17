a4d62ef124bb4aeadfe250f8a5990e9ac8c84b16
glee139 <glee00328@gmail.com>
2021-02-17 11:51:17 -0800

    created a new constructor in rectangle class and added a perimeter function and some getters

---
+++ src/rectangle.cpp
@@ -0,0 +1,30 @@
+include "../header/rectangle.hpp"
+
+void Rectangle::Rectangle(int w, int h){
+        this->width=-1;
+        this->height=-1;
+}
+
+void Rectangle::set_width(int w) {
+    this->width = w;
+}
+
+void Rectangle::set_height(int h) {
+    this->height = h;
+}
+
+int Rectangle::get_width() {
+    return this->width;
+}
+
+int Rectangle::get_height() {
+    return this->height;
+}
+
+int Rectangle::area() {
+    return -1;
+}
+
+int Rectangle::perimeter() {
+    return -1;
+}
